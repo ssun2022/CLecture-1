8f7bb122b31f4a590faa416cf74f7cb9f6007c6c
ksw617 <developer@overthehand.com>
2023-01-12 17:46:01 +0900

    (2023.1.12) 상수

---
+++ Variable/Source.c
@@ -0,0 +1,29 @@
+void main() // <- 주석
+{
+	// 변수란?
+	// 데이터를 저장할 수 있는 메모리 공간을
+	// 생성하는 것입니다.
+
+	// 자료형이란? 
+	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+
+	// char (문자) 1 byte
+
+	// 대입 연산자란?
+	// 변수에 값 또는 객체를 저장할 수 있는 연산자입니다.
+	char num1;
+
+	// short (정수) 2 byte
+	short num2 = 300;
+
+	// int (정수) 4 byte
+	int num3 = 400;
+
+	// long (정수) 4 byte
+
+	// float (실수) 4 byte
+	float num4 = 15.75;
+
+	// double (실수) 8 byte
+	// long double (실수) 8 and 12 byte 
+}
